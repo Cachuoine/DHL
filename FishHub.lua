9842744a6cece76def0f440f0bfc472c503bd6bf
Fish <daohuylam2205@gmail.com>
2026-07-18 19:20:09 +0700

    Update FishHub.lua

--- FishHub.lua
+++ FishHub.lua
@@ -1,3 +1,22 @@
 local encoded_url = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NhY2h1b2luZS9ESEwvcmVmcy9oZWFkcy9tYWluL0xvYWRpbmcubHVh"
-local decoded_url = base64_decode(encoded_url) 
-loadstring(game:HttpGet(decoded_url))()
+
+
+local function decodeBase64(data)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+
+local real_url = decodeBase64(encoded_url)
+loadstring(game:HttpGet(real_url))()
