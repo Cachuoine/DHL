6957f70a27b0a4be90e60dc4fdc79557c2b98ef9
Fish <daohuylam2205@gmail.com>
2026-07-20 22:17:47 +0700

    Update Setting.lua

--- Setting.lua
+++ Setting.lua
@@ -1,1 +1,20 @@
-
+local FishHub = getgenv().FishHub
+if not FishHub then
+    return
+end
+FishHub.ClearContent()
+local page = FishHub.pageContainer
+local UIS = game:GetService("UserInputService")
+textbox.Focused:Connect(function()
+    local conn
+    conn = UIS.InputBegan:Connect(function(input,gp)
+        if gp then return end
+        if input.UserInputType==Enum.UserInputType.Keyboard then
+            FishHub.HotkeyConfig.Key=input.KeyCode
+            textbox.Text=input.KeyCode.Name
+            textbox:ReleaseFocus()
+            conn:Disconnect()
+        end
+    end)
+end)
+HotkeyConfig.Key
