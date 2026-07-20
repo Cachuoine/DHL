8484e57955212cbf49a171d7b8052bd52e7b66f1
Fish <daohuylam2205@gmail.com>
2026-07-20 22:09:47 +0700

    Update Setting.lua

--- Setting.lua
+++ Setting.lua
@@ -1,5 +1,1 @@
-local Page = getgenv().FishHub.PageContainer
-local frame = Instance.new("Frame")
-frame.Parent = Page
-frame.Size = UDim2.new(1,0,1,0)
-frame.BackgroundColor3 = Color3.fromRGB(30,30,30)
+
